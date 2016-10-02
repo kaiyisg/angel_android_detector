5e10aa1f25f9e55f1ac3f0c14013f6041dd2d8ad
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-10-02 11:52:56 +0800

    getting ble address

--- app/src/main/java/electrical/nus/com/angeldetector/ui/MainActivity.java
+++ app/src/main/java/electrical/nus/com/angeldetector/ui/MainActivity.java
@@ -1,15 +1,43 @@
 package electrical.nus.com.angeldetector.ui;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import electrical.nus.com.angeldetector.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    String mBleDeviceAddress;
+    TextView bleaddressTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bleaddressTextView = (TextView)findViewById(R.id.bleaddressTextView);
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        Bundle extras = getIntent().getExtras();
+        assert(extras != null);
+        mBleDeviceAddress = extras.getString("ble_device_address");
+        bleaddressTextView.setText(mBleDeviceAddress);
+
+        //connect(mBleDeviceAddress);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        /*if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            displaySignalStrength(0);
+        }
+        unscheduleUpdaters();
+        mBleDevice.disconnect();*/
     }
 }
